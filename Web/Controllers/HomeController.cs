eb23396d68cdd1e094f33789d2e3edee32abdbd2
Степан Царьков <stepan.tsarkov.80@mail.ru>
2022-02-11 17:44:27 +0300

    exmpl1

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Moning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ContactForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FormContact(Contact contact)
+        {
+            using (ContactsContext db = new ContactsContext())
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+            }
+            return View("ContactAdded", contact);
+        }
+        public IActionResult ContactsList(string group = null)
+        {
+            List<Contact> c = null;
+            using (ContactsContext db = new ContactsContext())
+            {
+                if (group == null)
+                    ViewBag.Contacts = db.Contacts.ToList();
+                else if (group == "All")
+                    c = db.Contacts.ToList();
+                else
+                    c = db.Contacts.Where((e) => e.Group == group).ToList();
+            }
+            if (group == null)
+                return View();
+            else
+                return Json(c);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web/Models/ContactsContext .cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ContactsContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactsContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-238U1T3\SQLEXPRESS;Database=WebKus;Trusted_Connection=True;");
+        }
+    }
+
+}
